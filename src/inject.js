05a12347c56c6f5d667dfb9c1bc3d46f6776f40b
bumbread <thebumboni@gmail.com>
2022-06-21 00:55:43 +1100

    Add ability to restore page

--- src/inject.js
+++ src/inject.js
@@ -1,3 +1,14 @@
 
 body = document.getElementsByTagName('body')[0];
-body.setAttribute('style', 'max-width:80ch;margin:auto');
+// Hack but i don't care
+var old_style = body.getAttribute('_style');
+if(old_style == null) old_style = '';
+
+if(old_style != '') {
+    body.setAttribute('style', old_style);
+    body.setAttribute('_style', '');
+}
+else {
+    body.setAttribute('_style', body.getAttribute('style'));
+    body.setAttribute('style', 'max-width:80ch;margin:auto');
+}
